af2d7e8ffed49070364f6959645d2ac91f68dd13
suhanig <60145970+suhanig@users.noreply.github.com>
2022-01-24 09:28:27 -0500

    1.24.2022- added to be able to add guesses on each guess row.

--- Tester.java
+++ Tester.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Tester {
 
 	public static void main (String[] args) {
+		
+		int turns = 0;
 	
 
 		Mastermind m1 = new Mastermind ();
@@ -16,16 +18,21 @@ public class Tester {
 			//scanner class
 			char[] guess = new char [4];
 			Scanner scan = new Scanner (System.in);
-			System.out.println("welcome to Mastermind");
-			for (int x = 0; x < 4; x++) {
-				System.out.print("Enter letter for your guess: ");
-				guess[x] = scan.next().charAt(0);
-			}
-			System.out.println("Thanks for your guess...");
-			 m1.addGuess(guess);
+			System.out.println("Welcome to Mastermind. Please enter your guess by using letters for the colors. 'B' for blue, 'R' for red, 'Y' for yellow, and 'G' for green.");
+			for (int y = 0; y < 20; y+=2) {
+				
+				for (int x = 0; x < 4; x++) {
+					System.out.print("Enter letter for your guess: ");
+					guess[x] = scan.next().charAt(0);
+				}
+				//System.out.println("Thanks for your guess...");
+			
+			//System.out.println("Thanks for your guess...");
+			 m1.addGuess(guess, y);
 			//send it back
 			//m1.buildBoard();
 			m1.printBoard();
 			
+		}
 	} //close psvm
 }
