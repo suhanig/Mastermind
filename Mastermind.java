ee25a5992f419e7a89890da052b46f1c1d276fd5
suhanig <60145970+suhanig@users.noreply.github.com>
2022-01-24 09:27:28 -0500

    1.24.2022- made edits in class. increased board size and number of guesses

--- Mastermind.java
+++ Mastermind.java
@@ -18,35 +18,109 @@ public class Mastermind {
 	//declare memory spot
 	ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>> (); //creates placeholder in memory
 	
-	public void buildBoard () {
+	public void buildBoard () {//building the outline of the boar. try to put this into a for or wjile loop?
 			
-		board.add(new ArrayList<String>());  //board<list1, list2...>    board.get(0)  //'append'
 		
-		board.get(0).add(" "); 
-		board.get(0).add(" "); 		
-		board.get(0).add(" "); 
-		board.get(0).add(" ");
-		//now scoring
-		board.get(0).add("="); 
-		board.get(0).add(" "); 
-		board.get(0).add(" ");
-		board.add(new ArrayList<String>()); //create arraylist 4 second row
-		board.get(1).add("-"); 
-		board.get(1).add("-"); 		
-		board.get(1).add("-"); 
-		board.get(1).add("-");
-		//now scoring
-		board.get(1).add("="); 
-		board.get(1).add(" "); 
-		board.get(1).add(" ");
+		
+			for (int x = 0; x < 20; x+=2) {
+			board.add(new ArrayList<String>());  //board<list1, list2...>    board.get(0)  //'append'
+			board.get(x).add(" "); 
+			board.get(x).add(" "); 		
+			board.get(x).add(" "); 
+			board.get(x).add(" ");
+			//now scoring
+			board.get(x).add("="); 
+			board.get(x).add(" "); 
+			board.get(x).add(" ");
+			board.add(new ArrayList<String>()); //create arraylist 4 second row
+			//x++; 
+			board.get(x+1).add("-"); 
+			board.get(x+1).add("-"); 		
+			board.get(x+1).add("-"); 
+			board.get(x+1).add("-");
+			//now scoring
+			board.get(x+1).add("="); 
+			board.get(x+1).add(" "); 
+			board.get(x+1).add(" ");
+			
+			
+		} //closes for loop
+			/*board.get(2).add(" "); 
+			board.get(2).add(" "); 		
+			board.get(2).add(" "); 
+			board.get(2).add(" ");
+			//now scoring
+			board.get(2).add("="); 
+			board.get(2).add(" "); 
+			board.get(2).add(" ");
+			board.add(new ArrayList<String>()); //create arraylist 4 second row
+			board.get(3).add("-"); 
+			board.get(3).add("-"); 		
+			board.get(3).add("-"); 
+			board.get(3).add("-");
+			//now scoring
+			board.get(3).add("="); 
+			board.get(3).add(" "); 
+			board.get(3).add(" ");board.get(0).add(" "); 
+			board.get(4).add(" "); 		
+			board.get(4).add(" "); 
+			board.get(4).add(" ");
+			//now scoring
+			board.get(4).add("="); 
+			board.get(4).add(" "); 
+			board.get(4).add(" ");
+			board.add(new ArrayList<String>()); //create arraylist 4 second row
+			board.get(5).add("-"); 
+			board.get(5).add("-"); 		
+			board.get(5).add("-"); 
+			board.get(5).add("-");
+			//now scoring
+			board.get(5).add("="); 
+			board.get(5).add(" "); 
+			board.get(5).add(" ");
+			board.get(6).add(" "); 
+			board.get(6).add(" "); 		
+			board.get(6).add(" "); 
+			board.get(6).add(" ");
+			//now scoring
+			board.get(6).add("="); 
+			board.get(6).add(" "); 
+			board.get(6).add(" ");
+			board.add(new ArrayList<String>()); //create arraylist 4 second row
+			board.get(7).add("-"); 
+			board.get(7).add("-"); 		
+			board.get(7).add("-"); 
+			board.get(7).add("-");
+			//now scoring
+			board.get(7).add("="); 
+			board.get(7).add(" "); 
+			board.get(7).add(" ");
+			board.get(8).add(" "); 
+			board.get(8).add(" "); 		
+			board.get(8).add(" "); 
+			board.get(8).add(" ");
+			//now scoring
+			board.get(8).add("="); 
+			board.get(8).add(" "); 
+			board.get(8).add(" ");
+			board.add(new ArrayList<String>()); //create arraylist 4 second row
+			board.get(9).add("-"); 
+			board.get(9).add("-"); 		
+			board.get(9).add("-"); 
+			board.get(9).add("-");
+			//now scoring
+			board.get(9).add("="); 
+			board.get(9).add(" "); 
+			board.get(9).add(" "); */
+		
 	}
 	
 	//method to add test guess to board?
-	public void addGuess (char[] arrChar) {  // ['R', 'B', 'Y', 'G' ]
+	public void addGuess (char[] arrChar, int turns) {  // ['R', 'B', 'Y', 'G' ]
 		
 		for (int i = 0; i < 4; i++) {
 			//iterate thru each element in the [] arrchar
-			board.get(0).set(i, String.valueOf(arrChar[i]) );
+			board.get(turns).set(i, String.valueOf(arrChar[i]) );
 			//put each letter into the arrayList
 		}
 		
@@ -61,8 +135,17 @@ public class Mastermind {
 	
 	System.out.println(" Guesses \t Scores");
 	
-	System.out.println(board.get(0));
-	System.out.println(board.get(1));
+	for (int p=0; p<20; p++) {
+	System.out.println(board.get(p));
+	}
+	/*System.out.println(board.get(1));
+	System.out.println(board.get(3));
+	System.out.println(board.get(4));
+	System.out.println(board.get(5));
+	System.out.println(board.get(6));
+	System.out.println(board.get(7));
+	System.out.println(board.get(8));
+	System.out.println(board.get(9)); */
 	
 	}
 	
