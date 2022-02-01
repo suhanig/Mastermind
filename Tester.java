c825b258abeda1be680de80f33104ead09b4f720
suhanig <60145970+suhanig@users.noreply.github.com>
2022-02-01 13:00:43 -0500

    2.1.2022- things done in freebell. mostly all code done.

--- Tester.java
+++ Tester.java
@@ -1,21 +1,22 @@
 //Tester.java
 //import java.util.Scanner;
 
-public class Tester {
+public class Tester { //responsible for exectutioning the code written in the Mastermind header
 
-	public static void main (String[] args) {
+	public static void main (String[] args) { //opens psvm
 		
-		int turns = 0;
+		//int turns = 0;
 	
 
-		Mastermind m1 = new Mastermind ();
+		Mastermind m1 = new Mastermind (); //so tester can call on code from Mastermind.java
 		
 			//constuctor?? m1??
+			m1.rules(); //calls on method that explains and prints out the rules of the game 
+			//lays out structure of board. prints a black board out before starting the game
+			m1.buildBoard(); //calls on the buildBoard method
+			//m1.printBoard(); //calls on the printBoard method
 			
-			m1.buildBoard();
-			m1.printBoard();
-			
-			m1.secretCode();
+			m1.secretCode(); //calls on secretCode method
 			
 			//scanner class
 			/* char[] guess = new char [4];
@@ -33,7 +34,16 @@ public class Tester {
 			 //m1.addGuess();
 			//send it back
 			//m1.buildBoard();
-			m1.userGuess();
+			m1.userGuess(); //calls on userGuess method
 			
 		}
 	} //close psvm
+	
+	
+	
+	//THNGS LEFT TO DO:
+		//hide secret code. it shouldnt print out
+		//make sure rules are clear enough
+		//readMe file
+		//go throigh checklist/requirments on schoology
+	
