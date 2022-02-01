1e400ad5958a3a26bf71142116b8f46fd094f9d8
suhanig <60145970+suhanig@users.noreply.github.com>
2022-02-01 09:33:12 -0500

    1.1.2022- made some changes to tester. took out user input from this

--- Tester.java
+++ Tester.java
@@ -1,5 +1,5 @@
 //Tester.java
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Tester {
 
@@ -15,10 +15,12 @@ public class Tester {
 			m1.buildBoard();
 			m1.printBoard();
 			
+			m1.secretCode();
+			
 			//scanner class
-			char[] guess = new char [4];
+			/* char[] guess = new char [4];
 			Scanner scan = new Scanner (System.in);
-			System.out.println("Welcome to Mastermind. Please enter your guess by using letters for the colors. 'B' for blue, 'R' for red, 'Y' for yellow, and 'G' for green.");
+			System.out.println("Welcome to Mastermind. Please enter your guess by using letters for the colors. 'B' for blue, 'R' for red, 'Y' for yellow, 'G' for green, 'O' for orange, and 'P' for purple.");
 			for (int y = 0; y < 20; y+=2) {
 				
 				for (int x = 0; x < 4; x++) {
@@ -27,12 +29,11 @@ public class Tester {
 				}
 				//System.out.println("Thanks for your guess...");
 			
-			//System.out.println("Thanks for your guess...");
-			 m1.addGuess(guess, y);
+			//System.out.println("Thanks for your guess..."); */
+			 //m1.addGuess();
 			//send it back
 			//m1.buildBoard();
-			m1.printBoard();
+			m1.userGuess();
 			
 		}
 	} //close psvm
-}
